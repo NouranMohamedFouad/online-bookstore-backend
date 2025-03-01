36a6dc43bc2eb0a8bdb98350af43c2cf66d6624c
Mohamed Abusaif <67015050+Mohamed-Abusaif@users.noreply.github.com>
2025-03-01 22:30:22 +0200

    commit auth code

--- models/users.js
+++ models/users.js
@@ -1,6 +1,6 @@
-import process from 'node:process';
-import mongoose from 'mongoose';
-import AutoIncrementFactory from 'mongoose-sequence';
+import process from "node:process";
+import mongoose from "mongoose";
+import AutoIncrementFactory from "mongoose-sequence";
 
 const connection = mongoose.createConnection(process.env.DB_CONNECTION_STRING);
 const AutoIncrement = AutoIncrementFactory(connection);
@@ -8,74 +8,140 @@ const AutoIncrement = AutoIncrementFactory(connection);
 const userSchema = new mongoose.Schema({
   user_id: {
     type: Number,
-    min: [1, 'User ID must be at least 1']
+    min: [1, "User ID must be at least 1"],
   },
   name: {
     type: String,
-    required: [true, 'Name is required'],
+    required: [true, "Name is required"],
     trim: true,
-    minlength: [3, 'Name must be at least 3 characters'],
-    maxlength: [30, 'Name cannot exceed 30 characters'],
-    match: [/^[A-Z]+(\s[A-Z]+)*$/i, 'Name should contain only letters and must not be spaces only'],
-    set: (value) => value.replace(/\b\w/g, (char) => char.toUpperCase())
+    minlength: [3, "Name must be at least 3 characters"],
+    maxlength: [30, "Name cannot exceed 30 characters"],
+    match: [
+      /^[A-Z]+(\s[A-Z]+)*$/i,
+      "Name should contain only letters and must not be spaces only",
+    ],
+    set: (value) => value.replace(/\b\w/g, (char) => char.toUpperCase()),
   },
   email: {
     type: String,
-    required: [true, 'Email is required'],
+    required: [true, "Email is required"],
     trim: true,
     unique: true,
     lowercase: true,
-    match: [/^[\w.%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/i, 'Invalid email format']
+    match: [/^[\w.%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/i, "Invalid email format"],
   },
   password: {
     type: String,
-    required: [true, 'Password is required'],
-    minlength: [8, 'Password must be at least 8 characters long'],
-    maxlength: [32, 'Password must not exceed 32 characters'],
-    match: [/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,32}$/, 'Password must be 8-32 characters long, include at least one uppercase letter, one lowercase letter, one number, and one special character (@$!%*?&)']
+    required: [true, "Password is required"],
+    minlength: [8, "Password must be at least 8 characters long"],
+    maxlength: [32, "Password must not exceed 32 characters"],
+    match: [
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,32}$/,
+      "Password must be 8-32 characters long, include at least one uppercase letter, one lowercase letter, one number, and one special character (@$!%*?&)",
+    ],
   },
   role: {
     type: String,
-    enum: ['superadmin', 'admin', 'seller', 'delivery', 'customer'],
-    default: 'customer',
-    required: true
+    enum: ["superadmin", "admin", "seller", "delivery", "customer"],
+    default: "customer",
+    required: true,
   },
   address: {
     street: {
       type: String,
       trim: true,
-      minlength: [3, 'Street must be at least 3 characters long']
+      minlength: [3, "Street must be at least 3 characters long"],
     },
     city: {
       type: String,
-      match: [/^[A-Z\s]+$/i, 'City should contain only letters']
+      match: [/^[A-Z\s]+$/i, "City should contain only letters"],
     },
     state: {
       type: String,
-      match: [/^[A-Z\s]+$/i, 'State should contain only letters']
+      match: [/^[A-Z\s]+$/i, "State should contain only letters"],
     },
     postalCode: {
       type: String,
-      match: [/^\d{4,6}$/, 'Postal Code must be 4 to 6 digits']
+      match: [/^\d{4,6}$/, "Postal Code must be 4 to 6 digits"],
     },
     country: {
       type: String,
-      default: 'Egypt',
-      match: [/^[A-Z\s]+$/i, 'Country should contain only letters']
+      default: "Egypt",
+      match: [/^[A-Z\s]+$/i, "Country should contain only letters"],
     },
-    required: false
+    required: false,
   },
   phone: {
     type: String,
-    required: [true, 'Phone number is required'],
-    match: [/^\+?\d{7,15}$/, 'Invalid phone number format'],
-    trim: true
-  }
-
+    required: [true, "Phone number is required"],
+    match: [/^\+?\d{7,15}$/, "Invalid phone number format"],
+    trim: true,
+  },
 });
 
-userSchema.plugin(AutoIncrement, {inc_field: 'user_id'});
+userSchema.plugin(AutoIncrement, { inc_field: "user_id" });
 
-const users = mongoose.model('Users', userSchema);
+userSchema.pre("save", async function (next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified("password")) return next();
+
+  // Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // Delete passwordConfirm field
+  this.passwordConfirm = undefined;
+  next();
+});
+
+userSchema.pre("save", function (next) {
+  if (!this.isModified("password") || this.isNew) return next();
+
+  this.passwordChangedAt = Date.now() - 1000;
+  next();
+});
+
+userSchema.pre(/^find/, function (next) {
+  // this points to the current query
+  this.find({ active: { $ne: false } });
+  next();
+});
+
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
+  if (this.passwordChangedAt) {
+    const changedTimestamp = parseInt(
+      this.passwordChangedAt.getTime() / 1000,
+      10
+    );
+
+    return JWTTimestamp < changedTimestamp;
+  }
+
+  // False means NOT changed
+  return false;
+};
+
+userSchema.methods.createPasswordResetToken = function () {
+  const resetToken = crypto.randomBytes(32).toString("hex");
+
+  this.passwordResetToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+
+  // console.log({ resetToken }, this.passwordResetToken);
+
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+
+  return resetToken;
+};
+
+const users = mongoose.model("Users", userSchema);
 
 export default users;
--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,19 @@
-import express from 'express';
+import express from "express";
 
-import BooksRouter from './booksroute.js';
-<<<<<<< HEAD
-import ReviewRouter from './reviewRoutes.js';
-=======
-import cartRoutes from './cartRoutes.js';
-import OrdersRouter from './ordersRoute.js';
-import paymentRoutes from './paymentRoutes.js';
->>>>>>> 70bf6293b685227bba38bb8bda044a36f47a520c
+import BooksRouter from "./booksroute.js";
+import ReviewRouter from "./reviewRoutes.js";
+import cartRoutes from "./cartRoutes.js";
+import OrdersRouter from "./ordersRoute.js";
+import paymentRoutes from "./paymentRoutes.js";
+import AuthRouter from "./authRoutes.js";
 
 const router = express.Router();
 
-router.use('/books', BooksRouter);
-<<<<<<< HEAD
-router.use('/reviews', ReviewRouter);
-=======
-router.use('/cart', cartRoutes);
-router.use('/payment', paymentRoutes);
-router.use('/orders', OrdersRouter);
->>>>>>> 70bf6293b685227bba38bb8bda044a36f47a520c
+router.use("/books", BooksRouter);
+router.use("/reviews", ReviewRouter);
+router.use("/cart", cartRoutes);
+router.use("/payment", paymentRoutes);
+router.use("/orders", OrdersRouter);
+router.use("/auth", AuthRouter);
 
 export default router;
