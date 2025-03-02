f82c9808cb8c3889886aedda81a26ec751bd17e3
Ahmed-Sayed-1 <ahmed2853519521@gmail.com>
2025-03-02 22:59:17 +0200

    Edite the shcmavalidator file

--- middlewares/schemaValidator.js
+++ middlewares/schemaValidator.js
@@ -34,26 +34,6 @@ function validateData(validator, data) {
   
   return data;
 }
-function validatePartialData(validator, partialData) {
-  const dataToValidate = {...partialData};
-  const valid = validator(dataToValidate);
-
-  if (!valid) {
-    const relevantErrors = validator.errors.filter((error) => {
-      if (error.keyword === 'required' && !(error.params.missingProperty in partialData)) {
-        return false;
-      }
-      return true;
-    });
-
-    if (relevantErrors.length > 0) {
-      throw new Error(JSON.stringify(relevantErrors, null, 2));
-    }
-  }
-
-  console.log('Partial Data is Valid:', dataToValidate);
-  return dataToValidate;
-}
 
 function validatePartialData(validator, partialData) {
   const dataToValidate = { ...partialData };
@@ -76,5 +56,4 @@ function validatePartialData(validator, partialData) {
   return dataToValidate;
 }
 
-export {compileSchema, convertMongooseSchema, validateData,validatePartialData};
 export {compileSchema, convertMongooseSchema, validateData, validatePartialData};
