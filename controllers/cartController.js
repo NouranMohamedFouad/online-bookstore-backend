9f207c93e7a3d2feeee94545ec1f073fa1468bcd
Mohamed Abusaif <67015050+Mohamed-Abusaif@users.noreply.github.com>
2025-03-04 03:07:13 +0200

    update cart functionality

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,6 +1,8 @@
 import { asyncWrapper } from "../helpers/asyncWrapper.js";
 import { validateData } from "../middlewares/schemaValidator.js";
+import { Books } from "../models/books.js";
 import { Cart, validate } from "../models/cart.js";
+import { Users } from "../models/users.js";
 
 const getAll = asyncWrapper(async () => {
   const cart = await Cart.find({}).exec();
@@ -19,5 +21,90 @@ const deleteById = asyncWrapper(async (id) => {
   const cart = await Cart.findByIdAndDelete(id);
   return "Cart deleted successfully";
 });
+
+export const addItemToCart = async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) {
+    return res.status(400).json({ message: error.details[0].message });
+  }
+
+  const user = await Users.findById(req.body.userId);
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  const book = await Books.findById(req.body.bookId);
+  if (!book) {
+    return res.status(404).json({ message: "Book not found" });
+  }
+
+  let cart = await Cart.findOne({ userId: req.body.userId });
+  if (!cart) {
+    cart = new Cart({
+      userId: req.body.userId,
+      items: [],
+      total_price: 0,
+    });
+  }
+
+  const itemIndex = cart.items.findIndex(
+    (item) => item.bookId.toString() === req.body.bookId
+  );
+
+  if (itemIndex > -1) {
+    cart.items[itemIndex].quantity += req.body.quantity;
+    cart.items[itemIndex].price = req.body.price;
+  } else {
+    cart.items.push({
+      bookId: req.body.bookId,
+      quantity: req.body.quantity,
+      price: req.body.price,
+    });
+  }
+
+  cart.total_price = cart.items.reduce(
+    (sum, item) => sum + item.quantity * item.price,
+    0
+  );
+
+  await cart.save();
+  res.status(200).json({ status: "success", data: { cart } });
+};
+
+export const removeItemFromCart = async (req, res) => {
+  const { userId, bookId } = req.body;
+
+  const cart = await Cart.findOne({ userId });
+  if (!cart) {
+    return res.status(404).json({ message: "Cart not found" });
+  }
+
+  const itemIndex = cart.items.findIndex(
+    (item) => item.bookId.toString() === bookId
+  );
+
+  if (itemIndex > -1) {
+    cart.items.splice(itemIndex, 1);
+    cart.total_price = cart.items.reduce(
+      (sum, item) => sum + item.quantity * item.price,
+      0
+    );
+    await cart.save();
+    return res.status(200).json({ status: "success", data: { cart } });
+  } else {
+    return res.status(404).json({ message: "Item not found in cart" });
+  }
+};
+
+export const showCartItems = async (req, res) => {
+  const { userId } = req.params;
+
+  const cart = await Cart.findOne({ userId }).populate("items.bookId");
+  if (!cart) {
+    return res.status(404).json({ message: "Cart not found" });
+  }
+
+  res.status(200).json({ status: "success", data: { cart } });
+};
 
 export { create, deleteById, getAll };
--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,25 +1,35 @@
-import express from 'express';
-import {CartController} from '../controllers/index.js';
-import CustomError from '../helpers/customErrors.js';
-import {protect, restrictTo} from '../middlewares/authentication.js';
+import express from "express";
+import {
+  addItemToCart,
+  removeItemFromCart,
+  showCartItems,
+} from "../controllers/cartController.js";
+import { CartController } from "../controllers/index.js";
+import CustomError from "../helpers/customErrors.js";
+
+import { protect, restrictTo } from "../middlewares/authentication.js";
 
 const router = express.Router();
 
-router.get('/', protect, restrictTo('customer'), async (req, res, next) => {
+router.get("/", protect, restrictTo("customer"), async (req, res, next) => {
   const [err, data] = await CartController.getAll();
   if (err) return next(new CustomError(err.message, 500));
   res.json(data);
 });
-router.post('/', async (req, res, next) => {
+router.post("/", async (req, res, next) => {
   const [err, data] = await CartController.create(req.body);
   if (err) return next(new CustomError(err.message, 422));
   res.json(data);
 });
 
-router.delete('/:id', async (req, res, next) => {
+router.delete("/:id", async (req, res, next) => {
   const [err, data] = await CartController.deleteById(req.params.id);
   if (err) return next(new CustomError(err.message, 404));
   res.json(data);
 });
+
+router.post("/add", addItemToCart);
+router.delete("/remove", removeItemFromCart);
+router.get("/:userId", showCartItems);
 
 export default router;
